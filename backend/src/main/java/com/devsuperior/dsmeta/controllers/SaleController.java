a15ebaed41fe0d094b6cf1f56545aee44e7e2699
Renato Farias <santosrenato351@gmail.com>
2022-11-19 20:16:28 -0300

    Twilio sms

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -2,13 +2,11 @@ package com.devsuperior.dsmeta.controllers;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
+import com.devsuperior.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -19,6 +17,8 @@ import java.util.List;
 public class SaleController {
     @Autowired
     private SaleService service;
+    @Autowired
+    private SmsService smsService;
 
     //requisição get, que busca por vendedores
     @GetMapping
@@ -26,5 +26,10 @@ public class SaleController {
                                 @RequestParam(value="maxDate", defaultValue="") String maxDate,
                                 Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
+    }
+
+    @GetMapping("/notification/{id}")
+    public void notifySms(@PathVariable Long id) {
+        smsService.sendSms(id);
     }
 }
